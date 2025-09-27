c9d2787aba5e01dd72e3669ad500f8d9508aee0b
mihir Prasad bhuyan <mihirprasad62@gmail.com>
2025-09-27 15:29:35 +0530

    Getting stared with hello world

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Angular_Rivision');
+  name="Mihir Prasad Bhuyan"
 }
