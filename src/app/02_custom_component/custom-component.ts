a9cb09368b9f2111609590f9a440d00215e1c28b
mihir Prasad bhuyan <mihirprasad62@gmail.com>
2025-09-28 11:17:18 +0530

    custom component Completed

---
+++ src/app/02_custom_component/custom-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-component',
+  imports: [],
+  template:'<h1>Custom Component</h1>',
+  styles:'h1{color:green}'
+  
+})
+export class CustomComponent {
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Interpolation } from './01_interpolation/interpolation';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CustomComponent } from './02_custom_component/custom-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Interpolation],
+  imports: [RouterOutlet,Interpolation,CustomComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
